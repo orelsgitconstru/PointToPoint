0844d6058137ac024bbd332ee43b12c923495f05
orelsgit <o.zilberman@constru.ai>
2022-02-24 11:18:20 +0200

    Initial Commit

---
+++ PointToPointPOC/PointToPointPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointToPointPOCApp.swift
+//  PointToPointPOC
+//
+//  Created by Orel Zilberman on 24/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PointToPointPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
